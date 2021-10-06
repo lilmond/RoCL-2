3a40660131c4531face3ec2a9d63fabd90238984
lilmond <54730301+lilmond@users.noreply.github.com>
2021-10-06 09:04:03 +0000

    Create cookie_validator.py

---
+++ cookie_validator.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup as htmlparser
+import requests
+
+def check_token(token: str) -> None:
+    http = requests.get("https://www.roblox.com/home", cookies={".ROBLOSECURITY": token})
+    html = htmlparser(http.text, features="html.parser")
+
+    user = html.find("meta", {"name": "user-data"})
+
+    if not user:
+        print(f"\r\nERROR: invalid cookie\r\n")
+        return
+
+    info = "\r\n" \
+    f"Username: {user.get('data-name')}\r\n" \
+    f"User ID:  {user.get('data-userid')}\r\n" \
+    f"Premium:  {user.get('data-ispremiumuser')}\r\n" \
+    f"Underage: {user.get('data-isunder13')}\r\n" \
+    f"Account:  {user.get('data-created')}\r\n"
+
+    print(info)
+
+def main():
+    try:
+        while True:
+            token = input("Token: ").strip()
+            check_token(token)
+    except KeyboardInterrupt:
+        return
+
+if __name__ == "__main__":
+    main()
